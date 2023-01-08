91563395014b4eb805ee6945c2698a79de7e7937
Riccardo Cecchini <rcecchini.ds@gmail.com>
2023-01-08 12:49:14 +0000

    Update CPP.js

--- src/Languages/CPP.js
+++ src/Languages/CPP.js
@@ -4,25 +4,6 @@ module.exports = function(engine){
 
     let Interpreter = engine.Interpreter;
 
-    let sComment = Interpreter.Main.NewSwitch('comment', '<!--')
-    let sCommentEnd = sComment.NewSwitch('end', '-->').end()
-
-    let sTag = Interpreter.Main.NewSwitch('tag', '<')
-    let sTagDefine = sTag.NewSwitch('define', '!')
-    let sTagEnd = sTag.NewSwitch('end', '/')
-    let sTagClose = sTag.NewSwitch('close', '>').required()
-
-    sTagClose.catch = function(tag, div){
-        // Ok, fine, but now read the attributes
-        let mainTag = tag._parent;
-        let divs = mainTag.$IterateDivs();
-
-        // Then close the tag
-        if(Interpreter.tag.end || Interpreter.tag.define) {
-            Interpreter.tag = Interpreter.tag._parent
-        }
-
-        Interpreter.Return()
-    }
+    
 }
 
