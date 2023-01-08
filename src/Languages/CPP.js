16c0fc80ec1e9eb0e6f4df55ffaaa184579fc133
Riccardo Cecchini <rcecchini.ds@gmail.com>
2023-01-08 15:49:28 +0000

    ogni nostra azione è attentamente ponderata
    Questo per dire che ci sto mettendo una vita a fare qualsiasi cosa

--- src/Languages/CPP.js
+++ src/Languages/CPP.js
@@ -5,7 +5,7 @@ module.exports = function(engine){
     let Interpreter = engine.Interpreter;
 
     let sDirective = Interpreter.Main.NewSwitch('directive', '#')
-
+    let sNewLine = sDirective.NewSwitch('newLine', '\n')
 
 
 }
